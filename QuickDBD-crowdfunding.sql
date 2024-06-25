3f20f00389dce4804c471f48fa0fc8561af23c97
mattcat1221 <158774626+mattcat1221@users.noreply.github.com>
2024-06-25 17:51:55 -0400

    Update QuickDBD-crowdfunding.sql

--- QuickDBD-crowdfunding.sql
+++ QuickDBD-crowdfunding.sql
@@ -1,45 +1,5 @@
-﻿Campaign
+Campaign
 -
-CREATE TABLE IF NOT EXISTS Campaign (
-    CF_ID varchar(255) PRIMARY KEY NOT NULL,
-    Contact_ID varchar(255) NOT NULL,
-    Company_Name varchar(255) NOT NULL,
-    Description varchar(255) NOT NULL,
-    Goal numeric(10,2) NOT NULL,
-    Pledged numeric(10,2) NOT NULL,
-    Outcome varchar(255) NOT NULL,
-    Backers_Count int NOT NULL,
-    Country varchar(255) NOT NULL,
-    Currency varchar(255) NOT NULL,
-    Launch_Date date NOT NULL,
-    End_Date date NOT NULL,
-    Category_ID varchar(255) NOT NULL,
-    SubCategory_ID varchar(255) NOT NULL,
-    FOREIGN KEY (Contact_ID) REFERENCES Contacts(Contact_ID),
-    FOREIGN KEY (Category_ID) REFERENCES Category(Category_ID),
-    FOREIGN KEY (SubCategory_ID) REFERENCES Subcategory(Subcategory_ID)
-);
-
-CREATE TABLE IF NOT EXISTS Contacts (
-    Contact_ID varchar(255) PRIMARY KEY NOT NULL,
-    First_Name varchar(255) NOT NULL,
-    Last_Name varchar(255) NOT NULL,
-    Email varchar(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Category (
-    Category_ID varchar(255) PRIMARY KEY NOT NULL,
-    Categories varchar(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Subcategory (
-    Subcategory_ID varchar(255) PRIMARY KEY NOT NULL,
-    Subcategories varchar(255) NOT NULL
-);
-
-   
-   
-   
 CF_ID varchar PK
 Contact_ID varchar FK - Contacts.Contact_ID
 Company_Name varchar
